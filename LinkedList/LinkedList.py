c30cf4fefc06cdec15701173874c87acac6acafb
Camron Chermak <camronchermak@Camrons-MBP.attlocal.net>
2021-03-16 09:18:17 -0700

    Define the interface for the LinkedList

---
+++ LinkedList/LinkedList.py
@@ -0,0 +1,74 @@
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def add(self, value):
+        raise NotImplementedError('Need to implement')
+
+    def add(self, index, value):
+        raise NotImplementedError('Need to implement')
+
+    def add_all(self, values):
+        raise NotImplementedError('Need to implement')
+
+    def add_all(self, index, value):
+        raise NotImplementedError('Need to implement')
+
+    def add_first(self, value):
+        raise NotImplementedError('Need to implement')
+
+    def add_last(self, value):
+        raise NotImplementedError('Need to implement')
+
+    def clear(self):
+        raise NotImplementedError('Need to implement')
+
+    def contains(self, value):
+        raise NotImplementedError('Need to implement')
+
+    def element(self):
+        raise NotImplementedError('Need to implement')
+
+    def get(self, index):
+        raise NotImplementedError('Need to implement')
+
+    def get_first(self):
+        raise NotImplementedError('Need to implement')
+
+    def get_last(self):
+        raise NotImplementedError('Need to implement')
+
+    def index_of(self, value):
+        raise NotImplementedError('Need to implement')
+
+    def last_index_of(self, value):
+        raise NotImplementedError('Need to implement')
+
+    def peek(self):
+        raise NotImplementedError('Need to implement')
+
+    def poll(self):
+        raise NotImplementedError('Need to implement')
+
+    def remove(self):
+        raise NotImplementedError('Need to implement')
+
+    def remove(self, index):
+        raise NotImplementedError('Need to implement')
+
+    def remove(self, value):
+        raise NotImplementedError('Need to implement')
+
+    def set(self, index, value):
+        raise NotImplementedError('Need to implement')
+
+    def size(self):
+        raise NotImplementedError('Need to implement')
+
+    def to_list(self):
+        raise NotImplementedError('Need to implement')
+
+    def __str__(self):
+        raise NotImplementedError('Need to implement')
