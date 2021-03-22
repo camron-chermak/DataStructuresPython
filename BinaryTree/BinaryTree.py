66b5671ff84f5d3b0e5aac9a578ca8cae3073641
Camron Chermak <camronchermak@Camrons-MBP.attlocal.net>
2021-03-21 22:37:30 -0700

    Starter code for Binary Tree

---
+++ BinaryTree/BinaryTree.py
@@ -0,0 +1,34 @@
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+        self.num_nodes = 0
+
+    def add(self, value):
+        raise Exception('Not yet implemented')
+
+    def remove(self, value):
+        raise Exception('Not yet implemented')
+
+    def contains(self, value):
+        raise Exception('Not yet implemented')
+
+    def root(self):
+        raise Exception('Not yet implemented')
+
+    def depth(self):
+        raise Exception('Not yet implemented')
+
+    def size(self):
+        raise Exception('Not yet implemented')
+
+    def pre_order(self):
+        raise Exception('Not yet implemented')
+
+    def post_order(self):
+        raise Exception('Not yet implemented')
+
+    def in_order(self):
+        raise Exception('Not yet implemented')
+
+    def __str__(self):
+        raise Exception('Not yet implemented')
